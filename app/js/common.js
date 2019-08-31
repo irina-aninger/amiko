c25552891b72082cf55e75161898a4638f0383fa
irina_petrushenko <irina.aninger@gmail.com>
2019-08-31 19:55:44 +0300

    js and adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -9,6 +9,37 @@ volumeTabs.addEventListener('click', function (e) {
 });
 
 
+// product count
+
+let count = document.querySelector('#count'),
+    minus = document.querySelector('.minus'),
+    plus = document.querySelector('.plus');
+
+minus.addEventListener('click', function () {
+    if (count.value >= 1) {
+        count.value--
+    }
+})
+
+plus.addEventListener('click', function () {
+    count.value++
+})
+
+
+// product to top cart
+
+let countCart = document.querySelector('.top_cart .count'),
+    addCart = document.querySelector('.add_cart__btn');
+
+addCart.addEventListener('click', function () {
+    if (count.value >= 1) {
+        countCart.innerHTML++
+    }
+});
+
+
+
+
 
 
 
